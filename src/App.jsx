c4ba3b655c5a8d3b2a224733a9098a0cf0c322b8
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2021-03-17 12:40:22 +0000

    hooks based data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button } from "./components/Button";
 import { Error } from "./components/Error";
 
-class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { data: "" };
-  //   this.fetchData = this.fetchData.bind(this);
-  // }
+const App = () => {
+  const [data, setData] = useState("");
 
-  state = {
-    data: "",
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // always fetch data here.
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = () => {
+  const fetchData = () => {
     fetch("https://www.tronalddump.io/random/quote")
       .then((response) => {
         console.log(response);
         return response.json();
       })
-      .then((data) => this.setState({ data: data }));
+      .then((data) => setData(data));
   };
 
-  render() {
-    if (this.state.data.error) return <Error />;
+  if (data.error) return <Error />;
 
-    return (
-      <div>
-        <h1>Hello world</h1>
-        <Button fetchData={this.fetchData} />
-        <p>{this.state.data && this.state.data.value}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Button fetchData={fetchData} />
+      <p>{data && data.value}</p>
+    </div>
+  );
+};
 
 export default App;
